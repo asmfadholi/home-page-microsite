0871503ebab8dcfcec6623cd4e04252ee4c801ac
Mochammad Fadholi <market.fadholi@gmail.com>
2022-09-11 22:07:39 +0700

    feat: style not found page

--- src/pages/not-found/index.js
+++ src/pages/not-found/index.js
@@ -1,5 +1,7 @@
+import styles from "./index.module.css";
+
 function NotFound() {
-  return <div>Not Found</div>;
+  return <div className={styles.styNotFound}>Not Found...</div>;
 }
 
 export default NotFound;
